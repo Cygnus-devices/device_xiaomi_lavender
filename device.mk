4c10f7deefa524bb98ec50508e74ad7812ba1880
33bca <fabian.leutenegger@bluewin.ch>
2020-05-16 15:31:24 +0200

    ANT+ : Adapt to CAF

--- device.mk
+++ device.mk
@@ -132,11 +132,8 @@ PRODUCT_COPY_FILES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
+    antradio_app \
+    libantradio \
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -450,8 +447,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+#    telephony-ext
 
 # TextClassifier
 PRODUCT_PACKAGES += \
