1ed20c51e450bbf4932ca78fb65898f6333ba01c
SparXFusion <s2234nadar@gmail.com>
2021-09-08 14:10:37 +0530

    lavender: adapt to CygnusOS
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/p404_lavender.mk
+    $(LOCAL_DIR)/cygnus_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    p404_lavender-user \
-    p404_lavender-userdebug \
-    p404_lavender-eng
+    cygnus_lavender-user \
+    cygnus_lavender-userdebug \
+    cygnus_lavender-eng
--- cygnus_lavender.mk
+++ cygnus_lavender.mk
@@ -9,13 +9,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common p404 stuff
-$(call inherit-product, vendor/404/configs/common.mk)
+# Inherit some common CygnusRom stuff
+$(call inherit-product, vendor/cygnus/configs/common.mk)
+$(call inherit-product, device/qcom/common/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-P404_BUILDTYPE := RIPPA
-
-$(call inherit-product, vendor/google/pixel/config.mk)
-$(call inherit-product, vendor/google/gms/config.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -23,7 +20,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := p404_lavender
+PRODUCT_NAME := cygnus_lavender
 PRODUCT_MODEL := Redmi Note 7
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -33,4 +30,6 @@ TARGET_VENDOR_PRODUCT_NAME := lavender
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210605.005 7349499 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
+# Cygnus Official Tags
+CYGNUS_BUILD_TYPE := OFFICIAL
+WITH_GAPPS := true
