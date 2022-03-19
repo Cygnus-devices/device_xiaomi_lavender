393ae0f9269c5dfd64f212b3689373db8033feac
z3nitsu <lovepreetsg63@gmail.com>
2022-03-19 19:11:33 +0530

    Lavender: rename TARGET_KERNEL_CONFIG

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_DENSITY := 420
 
 # Kernel
-TARGET_KERNEL_CONFIG := lavender-perf_defconfig 
+TARGET_KERNEL_CONFIG := lavender_defconfig 
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 # Manifest
